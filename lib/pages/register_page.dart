c10319600c4e1a2d795ecb3f3004b90fbdb929b6
Noel George C J <noelgeorge2004@gmail.com>
2023-10-16 23:02:23 +0530

    register page added

---
+++ lib/pages/register_page.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:social_app/components/my_button.dart';
+import 'package:social_app/components/my_textfield.dart';
+
+class LoginPage extends StatelessWidget {
+
+  // text controllers
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+
+  LoginPage({super.key});
+
+  void login() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: Center(child: 
+      Padding(
+        padding: const EdgeInsets.all(25),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // logo
+            Icon(
+              Icons.person,
+              size: 80,
+              color: Theme.of(context).colorScheme.inversePrimary
+            ),
+      
+            const SizedBox(height: 25,),
+      
+            // app name
+            const Text(
+              "M I N I M A L", 
+              style: TextStyle(
+                fontSize: 20
+              )
+            ),
+      
+            const SizedBox(height: 50,),
+      
+            //email textfield
+            MyTextField(
+              hintText: "Email", 
+              obscureText: false, 
+              controller: emailController
+            ),
+
+            const SizedBox(height: 10,),
+            // password textfield
+            MyTextField(
+              hintText: "Password", 
+              obscureText: true, 
+              controller: passwordController
+            ),
+
+            const SizedBox(height: 10,),
+            // forgot password
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text("Forgot Password?", style: TextStyle(color: Theme.of(context).colorScheme.secondary),),
+              ],
+            ),
+
+            const SizedBox(height: 25,),
+
+            // sign in button
+            MyButton(
+              text: "Login", 
+              onTap: login
+            ),
+
+            const SizedBox(height: 25),
+      
+            // don't have an account? 
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Don't have an account?",
+                  style: TextStyle(
+                    color: Theme.of(context).colorScheme.inversePrimary
+                  )
+                ),
+                GestureDetector(
+                  onTap: () {},
+                  child: const Text(
+                    " Register now!",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold
+                    )
+                  ),
+                )
+              ],
+            )
+          ]
+          ),
+      ),
+      )
+    );
+  }
+}
