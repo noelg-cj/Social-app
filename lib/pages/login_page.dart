a47cf608e219694e2f6db6fdd65f0a80c072d9d8
Noel George C J <noelgeorge2004@gmail.com>
2023-10-16 14:28:28 +0530

    login page textinputs added

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,12 +1,75 @@
 import 'package:flutter/material.dart';
+import 'package:social_app/components/my_textfield.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+
+  // text controllers
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+
+  LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.background,
+      body: Center(child: 
+      Padding(
+        padding: const EdgeInsets.all(25),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // logo
+            Icon(
+              Icons.person,
+              size: 80,
+              color: Theme.of(context).colorScheme.inversePrimary
+            ),
+      
+            const SizedBox(height: 25,),
+      
+            // app name
+            const Text(
+              "M I N I M A L", 
+              style: TextStyle(
+                fontSize: 20
+              )
+            ),
+      
+            const SizedBox(height: 50,),
+      
+            //email textfield
+            MyTextField(
+              hintText: "Email", 
+              obscureText: false, 
+              controller: emailController
+            ),
+
+            const SizedBox(height: 10,),
+            // password textfield
+            MyTextField(
+              hintText: "Password", 
+              obscureText: true, 
+              controller: passwordController
+            ),
+
+            const SizedBox(height: 10,),
+            // forgot password
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text("Forgot Password?", style: TextStyle(color: Theme.of(context).colorScheme.secondary),),
+              ],
+            )
+            // sign in button
+      
+            // don't have an account? 
+      
+          ]
+          ),
+      ),
+      )
     );
   }
 }
