da1c30eab1617e6b3ef4184a1e86d4ab6ee0351b
Noel George C J <noelgeorge2004@gmail.com>
2023-10-17 21:05:58 +0530

    Added auth page

---
+++ lib/auth/auth.dart
@@ -0,0 +1,27 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:social_app/auth/login_or_register.dart';
+import '../pages/home_page.dart';
+
+class AuthPage extends StatelessWidget {
+  const AuthPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return const HomePage();
+          }
+
+          else {
+            return const LoginOrRegister();
+          }
+        }
+      )
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:social_app/auth/auth.dart';
 import 'package:social_app/auth/login_or_register.dart';
 import 'package:social_app/firebase_options.dart';
 import 'package:social_app/theme/dark_mode.dart';
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const LoginOrRegister(),
+      home: const AuthPage(),
       theme: lightMode,
       darkTheme: darkMode,
     );
