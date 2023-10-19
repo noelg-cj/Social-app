9e6b31fd6e35c05f707d78c22bc4bfd7abb33b65
Noel George C J <noelgeorge2004@gmail.com>
2023-10-20 00:36:45 +0530

    Project completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,6 @@
+import 'dart:ffi';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:social_app/components/my_drawer.dart';
 import 'package:social_app/components/my_post_button.dart';
@@ -51,6 +54,73 @@ class HomePage extends StatelessWidget {
                 )
               ],
             ),
+          ),
+
+          StreamBuilder(
+            stream: database.getPostsStream(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+
+              final posts = snapshot.data!.docs;
+
+              if (snapshot.data == null || posts.isEmpty) {
+                return const Center(
+                  child: Padding(
+                    padding: EdgeInsets.all(25),
+                    child: Text("No Posts..Post something!")
+                  ),
+                );
+              } 
+
+              return Expanded(
+                child: ListView.builder(
+                  itemCount: posts.length,
+                  itemBuilder: (context, index) {
+                    final post = posts[index];
+
+                    String message = post['PostMessage'];
+                    String userEmail = post['UserEmail'];
+
+                    return Padding(
+                      padding: const EdgeInsets.all(12),
+                      child: Container(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 20, 
+                          vertical: 15
+                        ),
+                        decoration: BoxDecoration(
+                          color: Theme.of(context).colorScheme.primary,
+                          borderRadius: BorderRadius.circular(12)
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              message,
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.w600
+                              ),
+                            ),
+                            const SizedBox(height: 5),
+                            Text(
+                              userEmail,
+                              style: TextStyle(
+                                color: Theme.of(context).colorScheme.inversePrimary
+                              )
+                            )
+                          ],
+                        )
+                      ),
+                    );
+                  }
+                ),
+              );
+            }
           )
         ],
       )
